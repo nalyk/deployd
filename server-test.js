2a3b3337fd552fa2686481c328296b96bd80c1ae
Ion (Nalyk) Calmîș <dev.ungheni@gmail.com>
2025-06-17 12:46:54 +0300

    fix: use connectionString in server-test

--- server-test.js
+++ server-test.js
@@ -4,7 +4,7 @@ const server  = deployd({
   port: 2403,
   env : process.env.NODE_ENV || 'development',
   db  : {
-    url: process.env.MONGO_URL ||
+    connectionString: process.env.MONGO_URL ||
          'mongodb+srv://deployd:RzGJ035A792Bt8d4@pulsmedia-mongodb-d506b061.mongo.ondigitalocean.com/deployd?tls=true&authSource=admin&retryWrites=true&w=majority'
   }
 });
